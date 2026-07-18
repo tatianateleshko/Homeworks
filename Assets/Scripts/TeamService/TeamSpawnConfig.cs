bc77aaf247f5cbe99041184c1d70678b48cc4a85
tatianateleshko <79082449+tatianateleshko@users.noreply.github.com>
2026-07-18 12:04:45 +0200

    Added team service and team config

---
+++ Assets/Scripts/TeamService/TeamSpawnConfig.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TeamSpawnConfig", menuName = "Scriptable Objects/TeamSpawnConfig")]
+public class TeamSpawnConfig : SerializedScriptableObject
+{
+    [SerializeField] private List<TeamSpawnData> _teamSpawnData;
+
+    public TeamSpawnData GetTeamSpawnData(string name)
+    {
+        if (_teamSpawnData == null)
+            return null;
+        foreach (var teamSpawnData in _teamSpawnData)
+        {
+            if (teamSpawnData.Name == name)
+                return teamSpawnData;
+        }
+    }
+}
+
+[Serializable]
+public class TeamSpawnData
+{
+    public string Name;
+    public bool IsEnemy;
+    public List<UnitData> Units;
+}
